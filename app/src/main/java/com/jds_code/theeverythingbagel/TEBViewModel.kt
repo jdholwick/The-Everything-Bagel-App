f47a2b5c948bd684208f0e06b3664b282b7296e7
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2022-11-26 15:01:34 -0700

    Put some variables with backing properties in TEBViewModel.kt.

--- app/src/main/java/com/jds_code/theeverythingbagel/TEBViewModel.kt
+++ app/src/main/java/com/jds_code/theeverythingbagel/TEBViewModel.kt
@@ -4,4 +4,16 @@ import androidx.lifecycle.ViewModel
 
 class TEBViewModel : ViewModel() {
 
+    // These had been in NewNoteFragment.kt but should now be here in TEBViewModel.
+    //  Also, we need these to have backing backing properties like this so the
+    //  _variable is editable and accessible only in TEBViewModel.
+    private var _noteTitle = "title"
+    val noteTitle: String
+        get() = _noteTitle
+
+    private var _noteBody = "words words words"
+    val noteBody: String
+        get() = _noteBody
+
+
 }
