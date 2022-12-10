a6e8c9efcce11e3ad008eb58013c945571c8c770
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2022-12-10 16:15:06 -0700

    Working on making connection between data entered and the Room DB.

--- app/src/main/java/com/jds_code/theeverythingbagel/TEBViewModel.kt
+++ app/src/main/java/com/jds_code/theeverythingbagel/TEBViewModel.kt
@@ -1,11 +1,14 @@
 package com.jds_code.theeverythingbagel
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.jds_code.theeverythingbagel.database.notes.Notes
+import com.jds_code.theeverythingbagel.database.notes.NotesDao
+import kotlinx.coroutines.launch
 
-class TEBViewModel : ViewModel() {
+// 12/10/2022 @ 13:15: '(private val notesDao: NotesDao)' is currently causing
+//  app to crash
+class TEBViewModel/*(private val notesDao: NotesDao)*/ : ViewModel() {
 
     // These had been in NewNoteFragment.kt but should now be here in TEBViewModel.
     //  Also, we need these to have backing backing properties like this so the
@@ -20,18 +23,50 @@ class TEBViewModel : ViewModel() {
         Log.d("TEBViewModel", "TEBViewModel has been created.")
     }
 
+    /*private fun insertNote(notes: Notes) {
+        viewModelScope.launch {
+            notesDao.insert(notes)
+        }
+    }*/
+
+    // This returns an instance of the [Notes] entity class with the note info entered.
+    //  This will be used to add a new note to the Notes database.
+    private fun getNewNoteEntry(noteTitle: String, noteBody: String): Notes {
+        return Notes(
+            noteTitle = noteTitle,
+            noteBody = noteBody
+        )
+    }
+
+    // Adds new note to the database
+    /*fun addNewNote(noteTitle: String, noteBody: String) {
+        val newNote = getNewNoteEntry(noteTitle, noteBody)
+        insertNote(newNote)
+    }*/
+
     // I'm not 100% certain, but I think this is the best way to save the note
     //  title and body the user inputs.
-    fun setNoteTitle(newNoteTitle: String) {
+    /*fun setNoteTitle(newNoteTitle: String) {
         _noteTitle.value = newNoteTitle
     }
 
     fun setNoteBody(newNoteBody: String) {
         _noteBody.value = newNoteBody
-    }
+    }*/
 
     override fun onCleared() {
         super.onCleared()
         Log.d("TEBViewModel", "TEBViewModel has been destroyed.")
     }
 }
+
+/*
+class NotesViewModelFactory(private val notesDao: NotesDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TEBViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TEBViewModel(notesDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}*/
